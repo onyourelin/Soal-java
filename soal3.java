4752eabec107c0cff7cc141ba7988a3bd0f10dc1
Elinda Nurfitri <148702069+onyourelin@users.noreply.github.com>
2024-01-06 13:25:11 +0800

    Add files via upload

---
+++ soal3.java
@@ -0,0 +1,11 @@
+public class soal3 {
+  public static void main(String[] args){
+  string[][] buah = {{"Alpukat"},{"Anggur"},{"Apel"}};
+
+  for (int i=0; i<3; i++){
+      for (int j=0; j<1; j++){
+          System.out.println(buah[i][j]);
+      }
+    }
+  }
+}
